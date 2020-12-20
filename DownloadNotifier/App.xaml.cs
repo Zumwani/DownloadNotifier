306b5ca341a033c179f9ce4de067b8995c6f3e7d
Zumwani <andreas.ingeholm@gmail.com>
2020-12-20 13:15:30 +0100

    Pushed project

---
+++ DownloadNotifier/App.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Toolkit.Uwp.Notifications;
+using Syroot.Windows.IO;
+using Windows.UI.Notifications;
+
+namespace DownloadNotifier
+{
+
+    partial class App : Common.Wpf.App
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            StartWatcher();
+            base.OnStartup(e);
+        }
+
+        void ViewDownloads(object sender, RoutedEventArgs e) =>
+            OpenUtility.ViewDownloadsFolder();
+
+        void Shutdown(object sender, RoutedEventArgs e) =>
+            Shutdown();
+
+        #region File system watcher
+
+        static FileSystemWatcher watcher;
+
+        static void StartWatcher()
+        {
+
+            watcher?.Dispose();
+
+            watcher = new FileSystemWatcher(KnownFolders.Downloads.Path) { EnableRaisingEvents = true };
+
+            watcher.Changed += OnFileChanged;
+            watcher.Error += (s, e) => StartWatcher();
+
+            ToastNotificationManagerCompat.OnActivated += (e) =>
+            {
+                if (e.Argument.StartsWith("View:"))
+                    OpenUtility.ViewInExplorer(e.Argument["View:".Length..]);
+                else if (e.Argument.StartsWith("Open:"))
+                    OpenUtility.Open(e.Argument["Open:".Length..]);
+                else if (e.Argument.StartsWith("OpenWith:"))
+                    OpenUtility.OpenWith(e.Argument["OpenWith:".Length..]);
+            };
+
+        }
+
+        static void OnFileChanged(object sender, FileSystemEventArgs e)
+        {
+
+            if (e.FullPath.EndsWith(".part"))
+                return;
+
+            var content = new ToastContentBuilder().
+                AddToastActivationInfo("Open:" + e.FullPath, ToastActivationType.Background).
+                SetToastScenario(ToastScenario.Reminder).
+                AddText("'" + e.Name + "' just got downloaded.").
+                AddButton("Open with...", ToastActivationType.Background, "OpenWith:" + e.FullPath).
+                AddButton("View in downloads", ToastActivationType.Background, "View:" + e.FullPath).
+                GetToastContent();
+
+            var toast = new ToastNotification(content.GetXml());
+
+            ToastNotificationManagerCompat.CreateToastNotifier().Show(toast);
+
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ DownloadNotifier/OpenUtility.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using Syroot.Windows.IO;
+
+namespace DownloadNotifier
+{
+
+    public static class OpenUtility
+    {
+
+        #region Pinvoke
+
+        [DllImport("shell32.dll", EntryPoint = "SHOpenWithDialog", CharSet = CharSet.Unicode)]
+        private static extern int SHOpenWithDialog(IntPtr hWndParent, ref OPENASINFO oOAI);
+
+        private struct OPENASINFO
+        {
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string cszFile;
+
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string cszClass;
+
+            [MarshalAs(UnmanagedType.I4)]
+            public OPEN_AS_INFO_FLAGS oaifInFlags;
+        }
+
+        [Flags]
+        private enum OPEN_AS_INFO_FLAGS
+        {
+            OAIF_ALLOW_REGISTRATION = 0x00000001,   // Show "Always" checkbox
+            OAIF_REGISTER_EXT = 0x00000002,   // Perform registration when user hits OK
+            OAIF_EXEC = 0x00000004,   // Exec file after registering
+            OAIF_FORCE_REGISTRATION = 0x00000008,   // Force the checkbox to be registration
+            OAIF_HIDE_REGISTRATION = 0x00000020,   // Vista+: Hide the "always use this file" checkbox
+            OAIF_URL_PROTOCOL = 0x00000040,   // Vista+: cszFile is actually a URI scheme; show handlers for that scheme
+            OAIF_FILE_IS_URI = 0x00000080    // Win8+: The location pointed to by the pcszFile parameter is given as a URI
+        }
+
+        #endregion
+
+        /// <summary>Opens the downloads folder in explorer.</summary>
+        public static void ViewDownloadsFolder() =>
+            Open(KnownFolders.Downloads.Path);
+
+        /// <summary>Opens the file or opens the folder in explorer.</summary>
+        public static void Open(string file)
+        {
+            if (File.Exists(file) || Directory.Exists(file))
+                Process.Start("explorer", file);
+        }
+
+        /// <summary>Opens the file using the open with dialog.</summary>
+        public static void OpenWith(string file)
+        {
+
+            if (!File.Exists(file))
+                return;
+
+            var info = new OPENASINFO
+            {
+                cszFile = file,
+                oaifInFlags = OPEN_AS_INFO_FLAGS.OAIF_ALLOW_REGISTRATION | OPEN_AS_INFO_FLAGS.OAIF_REGISTER_EXT | OPEN_AS_INFO_FLAGS.OAIF_EXEC
+            };
+
+            SHOpenWithDialog(IntPtr.Zero, ref info);
+
+        }
+
+        /// <summary>View and select the file or folder in explorer.</summary>
+        public static void ViewInExplorer(string file)
+        {
+
+            if (!(File.Exists(file) || Directory.Exists(file)))
+                return;
+
+            var args = string.Format("/e, /select, \"{0}\"", file);
+
+            var info = new ProcessStartInfo
+            {
+                FileName = "explorer",
+                Arguments = args
+            };
+
+            try
+            {
+                Process.Start(info);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                throw;
+            }
+
+        }
+
+    }
+
+}
